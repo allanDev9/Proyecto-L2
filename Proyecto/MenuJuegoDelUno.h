2370cec9b811470e5a57c31af18deadce221dead
Allan Peña <allanpenaguerra2003@gmail.com>
2024-04-06 01:34:47 -0600

    Arreglo Juego del uno

--- Proyecto/MenuJuegoDelUno.h
+++ Proyecto/MenuJuegoDelUno.h
@@ -11,11 +11,11 @@ class MenuJuego{
     private:
     public:
     void inciarJuegoDelUno(){
-        int cartas[108] = {1A, 2A, 3A, 4A, 5A, 6A, 7A, 8A, 9A, +2A, ReversaA, BloquearTurnoA,
-                            1R, 2R, 3R, 4R, 5R, 6R, 7R, 8R, 9R, +2R, ReversaR, BloquearTurnoR,
-                            1AA, 2AA, 3AA, 4AA, 5AA, 6AA, 7AA, 8AA, 9AA, +2AA, ReversaAA, BloquearTurnoAA,
-                            1V, 2V, 3V, 4V, 5V, 6V, 7V, 8V, 9V, +2V, ReversaV, BloquearTurnoV,
-                            +4, +4, +4, +4};
+        int cartas[108] = {'1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A', '+2A', 'ReversaA', 'BloquearTurnoA',
+                           '1R', '2R', '3R', '4R', '5R', '6R', '7R', '8R', '9R', '+2R', 'ReversaR', 'BloquearTurnoR',
+                           '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '+2AM', 'ReversaAM', 'BloquearTurnoAM',
+                           '1V', '2V', '3V', '4V', '5V', '6V', '7V', '8V', '9V', '+2V', 'ReversaV', 'BloquearTurnoV',
+                           '+4_1', '+4_2', '+4_3', '+4_4', 'CambioDeColor1', 'CambioDeColor2', 'CambioDeColor3', 'CambioDeColor4'};
     }
 
     void mostrarMenuJuego(){
