56813acd84242fbd79de63baa65cfccaafe47a9b
Allan Peña <allanpenaguerra2003@gmail.com>
2024-04-17 02:13:07 -0600

    Colores de cada carta

--- Proyecto/MenuJuegoDelUno.h
+++ Proyecto/MenuJuegoDelUno.h
@@ -17,8 +17,6 @@ class MenuJuego{
         const string GREEN = "\033[32m";
         const string YELLOW = "\033[33m";
         const string BLUE = "\033[34m";
-        const string MAGENTA = "\033[35m";
-        const string CYAN = "\033[36m";
         const string WHITE = "\033[37m";
 
     string getColor(const string& card) {
